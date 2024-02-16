1ebbb264f376767d4835c83389d91d64c96f414e
Tadeo Lozano <A326489@alumnos.uaslp.mx>
2024-02-16 08:20:56 -0600

    se elimino la traduccion

--- script.js
+++ script.js
@@ -1,61 +1,31 @@
+//TMBD
+
 const API_KEY = 'api_key=f983f1a73e02ec0fd3c6c40e9180ffa5';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const main = document.getElementById('main');
-    const peliModal = new bootstrap.Modal(document.getElementById('peliModal'));
+const main = document.getElementById('main');
 
-    main.addEventListener('click', (event) => {
-        const targetCard = event.target.closest('.peli-card');
-        if (targetCard) {
-            const movieId = targetCard.dataset.movieId; 
-            const movieDetailsURL = `${BASE_URL}/movie/${movieId}?${API_KEY}`; 
-
-            fetch(movieDetailsURL)
-                .then(res => res.json())
-                .then(movie => {
-                    const { title, vote_average, overview, release_date, original_language } = movie;
-
-                    // Traduce el resumen utilizando la API de Google Translate
-                    translate(overview, {to: 'es'}) // Traduce al español, puedes cambiar al idioma que necesites
-                        .then(translation => {
-                            const modalContent = document.getElementById('modalContent');
-                            modalContent.innerHTML = `
-                                <h5>Título: ${title}</h5>
-                                <p>Año de lanzamiento: ${release_date}</p>
-                                <p>Idioma original: ${original_language}</p>
-                                <p>Rating: ${vote_average}</p>
-                                <p>Resumen: ${translation.text}</p>
-                                <!-- Agrega más detalles de la película según sea necesario -->
-                            `;
-                            peliModal.show(); 
-                        })
-                        .catch(error => console.error('Error al traducir el resumen:', error));
-                })
-                .catch(error => console.error('Error al obtener los detalles de la película:', error));
-        }
-    });
-});
+getPelis(API_URL);
 
 function getPelis(url){
     fetch(url)
     .then(res => res.json())
     .then(data => {
+        console.log(data.results)
         showPelis(data.results);
     })
-    .catch(error => console.error('Error al obtener las películas:', error));
 }
 
 function showPelis(data){
     main.innerHTML = '';
 
     data.forEach(peli => {
-        const { id, title, poster_path, vote_average } = peli;
+        const { id, title, poster_path, vote_average, overview } = peli;
         const peliCard = document.createElement('div');
         peliCard.classList.add('peli-card');
-        peliCard.dataset.movieId = id; 
+        peliCard.dataset.movieId = id;
         peliCard.innerHTML = `
             <div class="card">
                 <img src="${IMG_URL + poster_path}" alt="${title}" class="card-img-top">
@@ -81,14 +51,82 @@ function getColor(vote){
     }
 }
 
-const translate = (text, { to }) => {
-    return new Promise((resolve, reject) => {
-        // Aquí deberías llamar a la API de Google Translate, 
-        // como no puedo realizar llamadas a la API aquí, la función solo devuelve un texto de ejemplo
-        const translatedText = `Translated text of "${text}" to ${to}`;
-        resolve({ text: translatedText });
-    });
-};
+document.addEventListener('DOMContentLoaded', function() {
+    const main = document.getElementById('main');
+    const peliModal = new bootstrap.Modal(document.getElementById('peliModal'));
 
-// Llama a la función getPelis para cargar las películas cuando se carga la página
-getPelis(API_URL);
+    main.addEventListener('click', (event) => {
+        const targetCard = event.target.closest('.peli-card');
+        if (targetCard) {
+            const movieId = targetCard.dataset.movieId; 
+            const movieDetailsURL = `${BASE_URL}/movie/${movieId}?${API_KEY}`; 
+
+            
+            fetch(movieDetailsURL)
+                .then(res => res.json())
+                .then(movie => {
+                    const { title, vote_average, overview, release_date, original_language } = movie;
+
+                    const modalContent = document.getElementById('modalContent');
+                    modalContent.innerHTML = `
+                        <h5>Título: ${title}</h5>
+                        <p>Año de lanzamiento: ${release_date}</p>
+                        <p>Idioma original: ${original_language}</p>
+                        <p>Rating: ${vote_average}</p>
+                        <p>Resumen: ${overview}</p>
+                        <!-- Agrega más detalles de la película según sea necesario -->
+                    `;
+
+                    peliModal.show(); 
+
+                    // Traduce el resumen al idioma deseado
+                    translateOverview(overview, 'es'); // Cambia 'es' al idioma deseado
+                })
+                .catch(error => console.error('Error al obtener los detalles de la película:', error));
+        }
+    });
+
+    // Evento de clic para el enlace "Peliculas"
+    const peliculasLink = document.getElementById('peliculasLink');
+    peliculasLink.addEventListener('click', scrollToCards);
+});
+
+// Función para desplazar suavemente hacia las cards
+function scrollToCards(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    const cardsSection = document.getElementById('main');
+    cardsSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Función para traducir el overview al idioma deseado
+function translateOverview(overview, targetLanguage) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://google-translator9.p.rapidapi.com/v2/translate',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '462a288322msh405fc9ea2d873dfp11d878jsn05ac89c7c840',
+            'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com'
+        },
+        processData: false,
+        data: JSON.stringify({
+            q: overview,
+            target: targetLanguage
+        })
+    };
+
+    // Realiza la solicitud para traducir el overview al idioma deseado
+    $.ajax(settings)
+        .done(function(response) {
+            const translatedOverview = response.data.translation;
+            // Actualiza el contenido del modal con el overview traducido
+            document.getElementById('modalContent').innerHTML += `
+                <p>Resumen traducido: ${translatedOverview}</p>
+            `;
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.error('Error al traducir el overview:', errorThrown);
+        });
+}
